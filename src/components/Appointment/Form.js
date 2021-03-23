be96388cd6ba0a3a6d50fc243d5080e214467830
cromwellgrim <rogers.dylan@protonmail.com>
2021-03-22 19:06:50 -0700

    created tests for Form.js

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import Button from "components/Button";
 export default function Form(props) {
 	const [name, setName] = useState(props.name || "");
 	const [interviewer, setInterviewer] = useState(props.interviewer || null);
+	const [error, setError] = useState("");
 
 	const reset = () => {
 		setName("");
@@ -21,6 +22,16 @@ export default function Form(props) {
 	// }
 
 	//interviewers={props.interviewers}
+
+	function validate() {
+		if ( name === "" ) {
+			setError("Student name cannot be blank");
+			return;
+		}
+		setError("")
+		props.onSave(name, interviewer);		
+	}
+
 	return (
 		<main className="appointment__card appointment__card--create">
 			<section className="appointment__card-left">
@@ -33,8 +44,10 @@ export default function Form(props) {
 						type="text"
 						placeholder="Enter Student Name"
 						onChange={(event) => setName(event.target.value)}
+						data-testid="student-name-input"
 					/>
 				</form>
+				<section className="appointment__validation">{error}</section>
 				<InterviewerList
 					interviewers={props.interviewers}
 					value={interviewer}
@@ -46,7 +59,7 @@ export default function Form(props) {
 					<Button danger onClick={cancel}>
 						Cancel
 					</Button>
-					<Button confirm onClick={() => props.onSave(name, interviewer)}>
+					<Button confirm onClick={validate}>
 						Save
 					</Button>
 				</section>
