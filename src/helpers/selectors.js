7c96c92fa4a93b9ad0bd2a8ec6e9f30cc1b7dec9
cromwellgrim <rogers.dylan@protonmail.com>
2021-03-24 17:15:52 -0700

    prettier'd helper files

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,40 +1,39 @@
 export function getAppointmentsForDay(state, day) {
-  const filterDays = [];
+	const filterDays = [];
 
-  state.days.forEach((daySched) => {
-    if (daySched.name === day) {
-      daySched.appointments.forEach((appt) => {
-        filterDays.push(state.appointments[appt])
-      })
-    }
-  })
+	state.days.forEach((daySched) => {
+		if (daySched.name === day) {
+			daySched.appointments.forEach((appt) => {
+				filterDays.push(state.appointments[appt]);
+			});
+		}
+	});
 
-  return filterDays;
-};
+	return filterDays;
+}
 
 export function getInterview(state, interview) {
+	if (!interview) {
+		return null;
+	}
+	const theInterview = {
+		interviewer: state.interviewers[interview.interviewer],
+		student: interview.student,
+	};
 
-  if (!interview) {
-    return null;
-  }
-  const theInterview = {
-    interviewer: state.interviewers[interview.interviewer], 
-    student: interview.student
-  }
-
-  return theInterview;
-};
+	return theInterview;
+}
 
 export function getInterviewersForDay(state, day) {
-  const interviewDays = [];
+	const interviewDays = [];
 
-  state.days.forEach((daySched) => {
-    if (daySched.name === day) {
-      daySched.interviewers.forEach((appt) => {
-        interviewDays.push(state.interviewers[appt])
-      })
-    }
-  })
+	state.days.forEach((daySched) => {
+		if (daySched.name === day) {
+			daySched.interviewers.forEach((appt) => {
+				interviewDays.push(state.interviewers[appt]);
+			});
+		}
+	});
 
-  return interviewDays;
-};
+	return interviewDays;
+}
